dbab267e26be0e6c1bb7742d2b0291f12c31994e
kashi <kashif@simplusinnovation.com>
2021-12-28 15:55:03 +0500

    route for blueprints and collections

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -9,13 +9,17 @@ import { json } from 'body-parser'
 import { Server } from 'http'
 import { log } from '../log'
 import { accessLog } from '../config/accessLogs'
+import { deckRouter} from '../collections'
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 /**
  * Server initialization and middlewares
  */
 export const app: any = express()
-
+app.locals.title = 'decks api'
+app.locals.email = 'm.kashi.khan93@gmail.com'
+app.locals.issues = 'https://github.com/democy/deck-microservice/issues'
+app.locals.baseUri = process.env.BASE_URI || '/api/v1/deck-api'
 
 
 app.use(json())
@@ -36,7 +40,11 @@ app.use('*', accessLog)
 /**
  * Server routing (Standard)
  */
- app.get(`/ping`, (_req: express.Request , res: express.Response) => {res.sendStatus(200)})
+ app.get(`${app.locals.baseUri}/ping`, (_req: express.Request , res: express.Response) => {res.sendStatus(200)})
+ app.use(`${app.locals.baseUri}/blueprint`, express.static('docs/blueprint/', {extensions : ['html'], index : 'index.html'}))
+ app.use(`${app.locals.baseUri}/documentation`, express.static('docs/typedoc/', {extensions : ['html'], index : 'index.html'}))
+ app.use(`${app.locals.baseUri}/tests`, express.static('docs/tests/', {extensions : ['html'], index : 'index.html'}))
+ app.use(`${app.locals.baseUri}/`, deckRouter());
  
 /////////////////////////////////////////////////////////////////////////////////////////////////
 /**
@@ -59,7 +67,7 @@ app.use(errorHandler)
 /**
  * Launch server
  */
-export const SERVER_PORT = parseInt(process.env.PORT || '3005')
+export const SERVER_PORT = parseInt(process.env.PORT || '3006')
 
 export const server: Server = app.listen( SERVER_PORT, '', () => {
 	log.debug('Server is running on port ', SERVER_PORT)
